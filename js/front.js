33360d5b930440d2007ce7d9a246b0163c544cc9
Francesco Bonadiman <franzbona@hotmail.it>
2015-12-05 21:34:24 +0100

    Edit text

--- js/front.js
+++ js/front.js
@@ -349,9 +349,9 @@ function map() {
     var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
 	el: '#map',
-    /* centered on Prague and zoomed out */
-	lat: 50.0833,
-	lng: 14.4167,
+    /* centered on Paris and zoomed out */
+  lat: 48.8567,
+  lng: 2.3508,
   zoom: 4,
 	zoomControl: true,
 	zoomControlOpt: {
